c9f01cd9f17657a986d6d9353683ef92747b0603
jurgen <jurgen.ukkivi@easypark.net>
2019-07-08 15:52:43 +0300

    Added empty field checks

--- src/main/resources/static/js/kontakt.js
+++ src/main/resources/static/js/kontakt.js
@@ -1,5 +1,30 @@
+$('input[type="text"]').bind("input propertychange", function () {
+    $(this).css('border', '');
+});
+
+$('#emailContent').bind("input propertychange", function () {
+    $(this).css('border', '');
+});
+
 $('#sendEmail').on('click', function() {
-    console.log("Lood");
+    var empty = false;
+    $('input[type="text"]').each(function(){
+        if ($(this).val().trim() === '') {
+            $(this).css('border', '1px solid red');
+            empty = true;
+            return false;
+        }
+    });
+
+    if (empty) {
+        return;
+    }
+
+    if (!$('#emailContent').val()) {
+        $('#emailContent').css('border', '1px solid red');
+        return false;
+    }
+
     fetch('/sendEmail', {
         headers: { "Content-Type": "application/json; charset=utf-8" },
         method: 'POST',
