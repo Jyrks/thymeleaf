293a97a9d0d04dc1a2ebdac4c4e8a8c1a959b3be
jurgen <jurgen.ukkivi@concise.ee>
2019-08-06 22:41:38 +0300

    New design

--- src/main/java/juust/PageController.java
+++ src/main/java/juust/PageController.java
@@ -38,7 +38,7 @@ public class PageController {
     @GetMapping("/")
     public String index(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
-        return "index";
+        return "index3";
     }
 
     @GetMapping("/koosta-vaagen.html")
